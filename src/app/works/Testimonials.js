dd9658580983f41963658990e6440576cc1bad8c
Anish949 <74363280+Anish949@users.noreply.github.com>
2023-09-05 23:08:59 +0545

    code fixes

--- src/app/works/Testimonials.js
+++ src/app/works/Testimonials.js
@@ -25,32 +25,32 @@ const Testimonials = () => {
           What our customers are saying about us
         </h1>
       </div>
-      <div class="flex flex-col items-center flex-wrap justify-center w-full gap-4p-8 mb-8 md:flex-row md:mb-0 flex-between">
+      <div className="flex flex-col items-center flex-wrap justify-center w-full gap-4p-8 mb-8 md:flex-row md:mb-0 flex-between">
         {orders[0] &&
           orders.map(({ Name, Post, Testimonial, ImageURL, id }) => {
             return (
               <Fade key={id}>
-                <div class="bg-orange-100 hover:scale-105 pointer w-72 shadow-lg mx-auto my-2 rounded-xl p-4">
-                  <p class="text-gray-800 font-medium text-md ">
-                    <span class="text-lg font-bold text-orange-500">“</span>
+                <div className="bg-orange-100 hover:scale-105 pointer w-72 shadow-lg mx-auto my-2 rounded-xl p-4">
+                  <p className="text-gray-800 font-medium text-md ">
+                    <span className="text-lg font-bold text-orange-500">“</span>
                     {Testimonial}
-                    <span class="text-lg font-bold text-orange-500">”</span>
+                    <span className="text-lg font-bold text-orange-500">”</span>
                   </p>
-                  <div class="flex items-center mt-4">
-                    <a href="#" class="relative block">
+                  <div className="flex items-center mt-4">
+                    <a href="#" className="relative block">
                       <Image
                         width={100}
                         height={100}
                         alt="profil"
                         src={ImageURL}
-                        class="mx-auto object-cover rounded-full h-10 w-10 "
+                        className="mx-auto object-cover rounded-full h-10 w-10 "
                       />
                     </a>
-                    <div class="flex flex-col justify-between ml-2">
-                      <span class="text-md font-semibold text-indigo-800">
+                    <div className="flex flex-col justify-between ml-2">
+                      <span className="text-md font-semibold text-indigo-800">
                         {Name}{" "}
                       </span>
-                      <span class="flex items-center text-sm font-semibold ">
+                      <span className="flex items-center text-sm font-semibold ">
                         {Post}{" "}
                       </span>
                     </div>
--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -18,33 +18,33 @@ const Gallery = () => {
     getImages();
   }, []);
   return (
-    <div class="bg-white py-6 sm:py-8 lg:py-12">
-      <div class="mx-auto max-w-screen-2xl px-4 md:px-8">
-        <div class="mb-4 flex items-center justify-between gap-8 sm:mb-8 md:mb-12">
-          <div class="flex items-center gap-12">
-            <h2 class="text-2xl font-bold text-gray-800 lg:text-3xl">
+    <div className="bg-white py-6 sm:py-8 lg:py-12">
+      <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
+        <div className="mb-4 flex items-center justify-between gap-8 sm:mb-8 md:mb-12">
+          <div className="flex items-center gap-12">
+            <h2 className="text-2xl font-bold text-gray-800 lg:text-3xl">
               Gallery
             </h2>
 
-            <p class="hidden max-w-screen-sm text-gray-500 md:block">
+            <p className="hidden max-w-screen-sm text-gray-500 md:block">
               Explore some of our Captivating Photos.
             </p>
           </div>
 
           <a
             href="#"
-            class="inline-block rounded-lg border bg-white px-4 py-2 text-center text-sm font-semibold text-gray-500 outline-none ring-indigo-300 transition duration-100 hover:bg-gray-100 focus-visible:ring active:bg-gray-200 md:px-8 md:py-3 md:text-base"
+            className="inline-block rounded-lg border bg-white px-4 py-2 text-center text-sm font-semibold text-gray-500 outline-none ring-indigo-300 transition duration-100 hover:bg-gray-100 focus-visible:ring active:bg-gray-200 md:px-8 md:py-3 md:text-base"
           >
             More
           </a>
         </div>
 
-        <div class="grid grid-cols-1 gap-4 sm:grid-cols-3 md:gap-6 xl:gap-8">
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-3 md:gap-6 xl:gap-8">
           {Images &&
             Images.map(({ Date, id, ImageURL, Title }) => (
               <div
                 key={id}
-                class="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-80"
+                className="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-80"
               >
                 <Image
                   width={500}
@@ -52,12 +52,12 @@ const Gallery = () => {
                   src={ImageURL || null}
                   loading="lazy"
                   alt="Unable to load image"
-                  class="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
+                  className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
                 />
 
-                <div class="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50"></div>
+                <div className="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50"></div>
 
-                <span class="relative flex ml-4 mb-3 flex-col text-sm text-white font-medium md:ml-5 md:text-lg w-full">
+                <span className="relative flex ml-4 mb-3 flex-col text-sm text-white font-medium md:ml-5 md:text-lg w-full">
                   {Title}
                   <Chip
                     variant="outlined"
